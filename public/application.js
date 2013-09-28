0b43001e9547b3ec70aacdd390bfc571d9be4c3b
Rick Cid <rick@hispand.com>
2013-09-28 12:44:36 -0700

    Removed Jquery alerts

--- public/application.js
+++ public/application.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
 function player_hit() {
   $(document).on("click", "form#hit_form input", function() {
-    alert("player hits!");
+    /*alert("player hits!")*/;
     $.ajax({
       type: 'POST',
       url: '/game/player/hit'
@@ -19,7 +19,7 @@ function player_hit() {
 
 function player_stay() {
   $(document).on("click", "form#stay_form input", function() {
-    alert("player stays!");
+    /*alert("player stays!")*/;
     $.ajax({
       type: 'POST',
       url: '/game/player/stay'
@@ -32,7 +32,7 @@ function player_stay() {
 
 function dealer_hit() {
   $(document).on("click", "form#dealer_hit input", function() {
-    alert("dealer hits!");
+    /*alert("dealer hits!")*/;
     $.ajax({
       type: 'POST',
       url: '/game/dealer/hit'
